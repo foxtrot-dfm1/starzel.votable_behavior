49951290e597a4f05c385c808c76aec233b1edd7
roman <kysilroman99@gmail.com>
2022-06-09 15:56:13 +0200

    browser viewlets add

---
+++ src/starzel/votable_behavior/browser/vote.py
@@ -0,0 +1,24 @@
+# encoding=utf-8
+from starzel.votable_behavior.interfaces import IVoting
+from zope.event import notify
+from zope.lifecycleevent import ObjectModifiedEvent
+from zope.publisher.browser import BrowserPage
+
+
+class Vote(BrowserPage):
+
+    def __call__(self, rating):
+        voting = IVoting(self.context)
+        voting.vote(rating, self.request)
+        notify(ObjectModifiedEvent(self.context, "A vote has been submitted"))
+        return "success"
+
+
+class ClearVotes(BrowserPage):
+
+    def __call__(self):
+        voting = IVoting(self.context)
+        voting.clear()
+        notify(ObjectModifiedEvent(self.context,
+                                   "All votes of this item have been removed"))
+        return "success"
