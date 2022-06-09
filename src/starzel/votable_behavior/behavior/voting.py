26ae3a4f85ad0200d3257880a8dcc3b06274e2b9
roman <kysilroman99@gmail.com>
2022-06-09 14:42:19 +0200

    Add voting functionality

---
+++ src/starzel/votable_behavior/behavior/voting.py
@@ -0,0 +1,85 @@
+from starzel.votable_behavior.interfaces import IVotable, IVoting
+from hashlib import md5
+from BTrees.OIBTree import OIBTree
+from BTrees.OOBTree import OOBTree
+from Products.CMFPlone.utils import safe_bytes
+from zope.annotation.interfaces import IAnnotations
+from zope.component import adapter
+from zope.interface import implementer
+
+
+# The key must be unique. Using the class name with the complete module name
+# is a good idea. But be careful, you might want to change the key if you move
+# the location to a different place. Else you won't find your own annotations
+KEY = "starzel.votable_behavior.behavior.voting.Vote"
+
+
+@implementer(IVoting)
+@adapter(IVotable)
+class Vote(object):
+    def __init__(self, context):
+        self.context = context
+        annotations = IAnnotations(context)
+        if KEY not in annotations.keys():
+            self.clear()
+        else:
+            self.annotations = annotations[KEY]
+
+    @property
+    def votes(self):
+        return self.annotations['votes']
+
+    @property
+    def voted(self):
+        return self.annotations['voted']
+
+    def _hash(self, request):
+        """
+        This hash can be tricked out by changing IP Adresses and might allow
+        only a single person of a big company to vote
+        """
+        hash = md5()
+        hash.update(safe_bytes(request.getClientAddr()))
+        for key in ["User-Agent", "Accept-Language",
+                    "Accept-Encoding"]:
+            val = safe_bytes(request.getHeader(key))
+            if val:
+                hash.update(val)
+        return hash.hexdigest()
+
+    def vote(self, vote, request):
+        if self.already_voted(request):
+            raise KeyError("You may not vote twice")
+        vote = int(vote)
+        self.annotations['voted'].insert(
+            self._hash(request),
+            len(self.annotations['voted']))
+        votes = self.annotations['votes']
+        if vote not in votes:
+            votes[vote] = 1
+        else:
+            votes[vote] += 1
+
+    def total_votes(self):
+        return sum(self.annotations.get('votes', {}).values())
+
+    def average_vote(self):
+        total_votes = sum(self.annotations.get('votes', {}).values())
+        if total_votes == 0:
+            return 0
+        total_points = sum([vote * count for (vote, count) in
+                            self.annotations.get('votes', {}).items()])
+        return float(total_points) / total_votes
+
+    def has_votes(self):
+        return len(self.annotations.get('votes', {})) != 0
+
+    def already_voted(self, request):
+        return self._hash(request) in self.annotations['voted']
+
+    def clear(self):
+        annotations = IAnnotations(self.context)
+        annotations[KEY] = OOBTree()
+        annotations[KEY]['voted'] = OIBTree()
+        annotations[KEY]['votes'] = OOBTree()
+        self.annotations = annotations[KEY]
