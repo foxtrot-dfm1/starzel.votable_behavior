4b3c0216dffea3b25d481cb5d55608eb57cb8c3c
roman <kysilroman99@gmail.com>
2022-06-09 14:51:46 +0200

    Add RESTApi endpoins

--- src/starzel/votable_behavior/__init__.py
+++ src/starzel/votable_behavior/__init__.py
@@ -3,3 +3,7 @@
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('starzel.votable_behavior')
+
+
+DoVote = 'starzel.votable_behavior: Do Vote'
+ViewVote = 'starzel.votable_behavior: View Vote'
